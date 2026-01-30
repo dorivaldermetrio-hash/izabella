6bf63e0f9a6cf5f072910f3f6f87a3f95dd31254
dorivaldermetrio-hash <dorivaldermetrio@gmail.com>
2026-01-30 17:30:57 -0300

    ajuste contato crm

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -5,7 +5,7 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function WhatsAppButton() {
   return (
     <a
-      href="http://wa.me/554196647152"
+      href="http://wa.me/554188980887"
       target="_blank"
       rel="noopener noreferrer"
       className="fixed bottom-6 right-6 z-50 w-14 h-14 md:w-16 md:h-16 bg-[#25D366] rounded-full flex items-center justify-center shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-110 group"
